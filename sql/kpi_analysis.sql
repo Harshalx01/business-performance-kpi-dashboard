cc73329c5d9c64b7530652ad1c8b34270dda607c
Harsahl Rajgadkar <hrajgadkar@gmail.com>
2026-01-04 13:06:09 +0530

    Add SQL folder and KPI analysis file

---
+++ sql/kpi_analysis.sql
@@ -0,0 +1,8 @@
+-- Business Performance KPI Analysis
+-- This file contains SQL queries used to calculate
+-- key sales metrics, promotion impact, and top products.
+
+-- Total Sales Volume
+-- Promotional vs Non-Promotional Sales
+-- Average Sales per Product
+-- Top 5 Products by Sales
